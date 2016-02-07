eedfa7e298f65f102562cb0318bc4c2f8b119ebc
Osvaldas Grigas <o.grigas@gmail.com>
2016-02-07 11:37:48 +0200

    upgrade libs

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject clojure-intro "0.1"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [quil "2.2.2"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [quil "2.3.0"]])
