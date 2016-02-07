fbf9c4349dce068dffad742215abc3551bfe8a48
Osvaldas Grigas <o.grigas@gmail.com>
2016-02-07 11:38:50 +0200

    slowly move the camera to catch up with infinite growth

--- src/game/graphics.clj
+++ src/game/graphics.clj
@@ -6,27 +6,34 @@
 (def initial-cells (cell-patterns :infinite-growth))
 
 (defn setup-grid! []
-  (smooth)
-  (stroke 100 100 100)
+  (frame-rate 10)
+  (stroke 0)
   (stroke-weight 2)
-  (fill 255 255 255)
-  (frame-rate 12)
-  initial-cells)
+  (fill 255)
+  {:cells initial-cells
+   :camera [-130 -130]})
 
-(defn draw-frame! [live-cells]
-  (background 0 0 0)
-  (let [size 20]
-    (doseq [x (range 0 (width) size)]
-      (line x 0 x (height)))
-    (doseq [y (range 0 (height) size)]
-      (line 0 y (width) y))
-    (doseq [[x y] live-cells]
-      (rect (* x size) (* y size) size size))))
+(defn update-state [state]
+  (-> state
+      (update :cells next-generation)
+      (update-in [:camera 0] #(+ 0.8 %))
+      (update-in [:camera 1] #(+ 0.8 %))))
+
+(defn draw-frame! [{:keys [cells camera]}]
+  (background 0)
+  (with-translation [camera]
+    (let [size 10]
+      (doseq [x (range 0 (width) size)]
+        (line x 0 x (height)))
+      (doseq [y (range 0 (height) size)]
+        (line 0 y (width) y))
+      (doseq [[x y] cells]
+        (rect (* x size) (* y size) size size)))))
 
 (defsketch animation
            :setup setup-grid!
            :draw draw-frame!
-           :update next-generation
+           :update update-state
            :title "Game of Life"
-           :size [800 500]
+           :size [500 500]
            :middleware [fun-mode])
