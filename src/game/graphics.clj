b1a3b290920f1fbd54f16d0251cd0a7b21e0e246
Osvaldas Grigas <o.grigas@gmail.com>
2016-02-13 16:45:24 +0200

    don't :refer :all

--- src/game/graphics.clj
+++ src/game/graphics.clj
@@ -1,39 +1,39 @@
 (ns game.graphics
-  (:require [quil.core :refer :all]
-            [quil.middlewares.fun-mode :refer :all]
-            [game.logic :refer :all]))
+  (:require [quil.core :as q]
+            [quil.middlewares.fun-mode :refer [fun-mode]]
+            [game.logic :as game]))
 
-(def initial-cells (cell-patterns :infinite-growth))
+(def initial-cells (game/cell-patterns :infinite-growth))
 
 (defn setup-grid! []
-  (frame-rate 10)
-  (stroke 0)
-  (stroke-weight 2)
-  (fill 255)
+  (q/frame-rate 10)
+  (q/stroke 0)
+  (q/stroke-weight 2)
+  (q/fill 255)
   {:cells initial-cells
    :camera [-130 -130]})
 
 (defn update-state [state]
   (-> state
-      (update :cells next-generation)
+      (update :cells game/next-generation)
       (update-in [:camera 0] #(+ 0.8 %))
       (update-in [:camera 1] #(+ 0.8 %))))
 
 (defn draw-frame! [{:keys [cells camera]}]
-  (background 0)
-  (with-translation [camera]
+  (q/background 0)
+  (q/with-translation [camera]
     (let [size 10]
-      (doseq [x (range 0 (width) size)]
-        (line x 0 x (height)))
-      (doseq [y (range 0 (height) size)]
-        (line 0 y (width) y))
+      (doseq [x (range 0 (q/width) size)]
+        (q/line x 0 x (q/height)))
+      (doseq [y (range 0 (q/height) size)]
+        (q/line 0 y (q/width) y))
       (doseq [[x y] cells]
-        (rect (* x size) (* y size) size size)))))
+        (q/rect (* x size) (* y size) size size)))))
 
-(defsketch animation
-           :setup setup-grid!
-           :draw draw-frame!
-           :update update-state
-           :title "Game of Life"
-           :size [500 500]
-           :middleware [fun-mode])
+(q/defsketch animation
+             :setup setup-grid!
+             :draw draw-frame!
+             :update update-state
+             :title "Game of Life"
+             :size [500 500]
+             :middleware [fun-mode])
